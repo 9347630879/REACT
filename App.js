781b251be6f6510e79dd3d9107ab5af46fb3a10c
9347630879 <145848207+9347630879@users.noreply.github.com>
2023-11-28 12:50:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+
+// import { useState,useEffect } from "react";
+// import { Moviename } from "./components/Moviename";
+
+// import { Orders } from "./components/orders.components"
+// import  {Product} from "./components/Product.components";
+// import { Form } from "./components/form components";
+//import { Menu } from "./menu/menus.component";
+//  import './App.css'
+
+// import { BrowserRouter } from "react-router-dom";
+
+// import { AppRouter } from './app.route';
+
+
+//  import { Menuclass } from "./menu/menu.class.components";
+// import { Payslips } from "./payslip";
+import { Receipe } from "./Receipe";
+
+function App() {
+
+  return (
+    <div>
+      
+      {/* <Orders></Orders> */}
+      {/* <Form></Form> */}
+      {/* <Product></Product>  */}
+      {/* <Menu/>  */}
+      {/* <BrowserRouter>  */}
+      {/* <Menuclass></Menuclass>  */}
+      {/* <AppRouter/> */}
+      {/* </BrowserRouter> */}
+      {/* <Moviename/> */}
+      {/* <Payslips /> */}
+        
+       <Receipe></Receipe>
+
+    </div>
+
+  )
+}
+export default App;
